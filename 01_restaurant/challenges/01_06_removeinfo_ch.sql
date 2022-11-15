87ce8f96d82fd5f9cd459b2a210a5271d65b1df0
MmmarRTHa <46696166+MmmarRTha@users.noreply.github.com>
2022-11-15 05:54:09 +0000

    Solved sql challanges with medium difficulty.

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,18 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT * FROM Customers WHERE FirstName = 'Norby';
+
+SELECT * FROM Reservations WHERE CustomerID = 64 
+ORDER BY Date DESC;
+
+DELETE FROM Reservations WHERE ReservationID = 2000;
+
+-- or soft delete:
+UPDATE Reservations SET Date = NULL WHERE ReservationID = 2000;
+
+-- better solution with a join to retrieve the reservation ID
+SELECT * FROM Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE Customers.FirstName = 'Norby' AND Reservations.CustomerID = 64;
